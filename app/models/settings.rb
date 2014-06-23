bbd1e353e252bdd1f45dd8a58bf55c0c47674f77
Jakub Naliwajek <jakub.naliwajek@netguru.co>
2014-06-23 11:43:59 +0200

    add Settings model
    [#69344414]

---
+++ app/models/settings.rb
@@ -0,0 +1,21 @@
+class Settings
+  include Singleton
+  include Mongoid::Document
+
+  field :notifications_email, type: String
+
+  validate :notifications_email, format: { with: Devise.email_regexp }
+
+  class << self
+    delegate :inspect, to: :instance
+
+    def method_missing method, *args
+      instance.send(method, *args)
+    end
+
+    def instance
+      @instance ||= self.first_or_create
+    end
+  end
+end
+
