4f765cab797c4dbf75caa9e7534b1901f8809f12
Jakub Naliwajek <jakub.naliwajek@netguru.co>
2014-06-25 08:44:49 +0200

    better settings_spec description
    fix [3e9766e1fd9806dbae9a627a62d681d68e1b6ed7]

--- spec/models/settings_spec.rb
+++ spec/models/settings_spec.rb
@@ -2,9 +2,15 @@ require 'spec_helper'
 
 describe Settings do
 
-  it '#notifications_email' do
-    Settings.instance.notifications_email = 'foobar@example.com'
-    expect(Settings.notifications_email).to eq 'foobar@example.com'
+  context 'without any Settings instace initialized yet' do
+    it 'returns unset notifications_email' do
+      expect(Settings.notifications_email).to eq nil
+    end
+
+    it 'sets the notifications_email' do
+      Settings.instance.notifications_email = 'foobar@example.com'
+      expect(Settings.notifications_email).to eq 'foobar@example.com'
+    end
   end
 end
 
