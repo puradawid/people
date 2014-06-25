ebe12afcf762159868bb49a04f3613771beaef35
Jakub Naliwajek <jakub.naliwajek@netguru.co>
2014-06-25 08:50:43 +0200

    settings_specs on instances should be in one example

--- spec/models/settings_spec.rb
+++ spec/models/settings_spec.rb
@@ -3,11 +3,9 @@ require 'spec_helper'
 describe Settings do
 
   context 'without any Settings instace initialized yet' do
-    it 'returns unset notifications_email' do
-      expect(Settings.notifications_email).to eq nil
-    end
 
     it 'sets the notifications_email' do
+      expect(Settings.notifications_email).to eq nil
       Settings.instance.notifications_email = 'foobar@example.com'
       expect(Settings.notifications_email).to eq 'foobar@example.com'
     end
