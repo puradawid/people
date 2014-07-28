476591af0c867c70c82f228848027c30856f0282
Kuba Łasecki <kuba.lasecki@netguru.pl>
2014-07-28 10:25:37 +0200

    check emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < BaseMailer
   def notify_operations(email)
     @email = email
-    to = [AppConfig.emails.pm]
+    to = AppConfig.emails.support
     mail(to: to, subject: "New user in the application.", email: @email)
   end
 end
