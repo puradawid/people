71270d172e7a815028d4a9737cca035f85597343
HarenBroog <damian.romanow@netguru.co>
2014-07-31 17:01:52 +0200

    Team model validation
    [#75690346]

--- app/models/team.rb
+++ app/models/team.rb
@@ -5,6 +5,8 @@ class Team
   field :name
   field :team_leader_id
 
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+
   has_many :users
   accepts_nested_attributes_for :users
 end
