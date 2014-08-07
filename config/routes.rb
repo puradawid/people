c8d0fa088a7a62b2ba032bd076dd5f6e4c8d2fee
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-08-07 07:42:05 +0200

    init styleguide pages [#76425958]

--- config/routes.rb
+++ config/routes.rb
@@ -42,4 +42,9 @@ Hrguru::Application.routes.draw do
   get '/settings', to: 'settings#edit'
   root 'welcome#index'
   get '/github_connect', to: 'welcome#github_connect'
+
+  scope '/styleguide' do
+    get '/css', to: 'pages#css'
+    get '/components', to: 'pages#components'
+  end
 end
