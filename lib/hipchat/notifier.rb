9df1bfeb55f10a32dcb79adceced92433833c7b0
Jakub Naliwajek <jakub.naliwajek@netguru.co>
2014-08-07 14:23:38 +0200

    HipChat::Notifier#send_notifications method
    [#68095508]

--- lib/hipchat/notifier.rb
+++ lib/hipchat/notifier.rb
@@ -1,2 +1,17 @@
 class HipChat::Notifier
+  attr_reader :client
+
+  def initialize
+    @client = HipChat::Client.new(AppConfig.hipchat.api_token)
+  end
+
+  def send_notification(message)
+    @client[AppConfig.hipchat.room_name].send(
+      AppConfig.hipchat.author_name,
+      message,
+      notify: AppConfig.hipchat.notify_users,
+      color: AppConfig.hipchat.message_color
+    )
+  end
 end
+
--- spec/lib/hipchat/notifier_spec.rb
+++ spec/lib/hipchat/notifier_spec.rb
@@ -1,4 +1,7 @@
 require 'spec_helper'
 
 describe HipChat::Notifier do
+  subject { HipChat::Notifier.new }
+  it { should respond_to(:send_notification).with(1).argument }
 end
+
