f57016d8e7502a9d6baa5b7401cba347f2c2451c
testbrian <testbrian@users.noreply.github.com>
2014-08-19 18:15:25 +0100

    remove parenthesis in specs [#75691794]

--- spec/models/membership_spec.rb
+++ spec/models/membership_spec.rb
@@ -95,7 +95,7 @@ describe Membership do
     end
 
     it "returns memberships ending in next 30 days" do
-      membership = create(:membership, ends_at: 1.week.from_now)
+      membership = create :membership, ends_at: 1.week.from_now
       membership1 = create :membership, ends_at: 5.weeks.from_now
       expect(subject).to include membership
       expect(subject).not_to include membership1
@@ -112,7 +112,7 @@ describe Membership do
     end
 
     it "shows one user is joining" do
-      membership = create(:membership, starts_at: 1.week.from_now)
+      membership = create :membership, starts_at: 1.week.from_now
       expect(subject).to include membership
     end
   end
@@ -122,10 +122,10 @@ describe Membership do
     subject { Membership.upcoming_changes(days).to_a.flatten }
 
     it "includes 2 upcoming changes" do
-      membership1 = create(:membership, starts_at: 1.week.from_now)
-      membership2 = create(:membership, starts_at: 1.week.from_now)
-      membership_not_included = create(:membership, starts_at: 1.year.from_now,
-                                                    ends_at: 2.years.from_now)
+      membership1 = create :membership, starts_at: 1.week.from_now
+      membership2 = create :membership, starts_at: 1.week.from_now
+      membership_not_included = create :membership, starts_at: 1.year.from_now,
+                                                    ends_at: 2.years.from_now
       expect(subject).to include membership1, membership2
       expect(subject).not_to include membership_not_included
     end
