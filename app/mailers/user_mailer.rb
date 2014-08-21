8a8f97c754fe863fecf1217dcbb1f3607e4819c4
Mateusz Stebnicki <mateusz.stebnicki@netguru.pl>
2014-08-21 16:43:02 +0200

    fix UserMailer
    [#77375660]

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < BaseMailer
   def notify_operations(email)
     @email = email
-    to = AppConfig.emails.support
+    to = [AppConfig.emails.support, Settings.notifications_email].compact.uniq
     mail(to: to, subject: "New user in the application.", email: @email)
   end
 end
