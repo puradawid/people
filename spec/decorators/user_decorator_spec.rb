6b782b40ecaf35c568cf0e445151aeea100eee68
Marek Buszman <mbusiq@gmail.com>
2014-09-10 14:48:22 +0200

    user decorator specs pt1

--- spec/decorators/user_decorator_spec.rb
+++ spec/decorators/user_decorator_spec.rb
@@ -4,6 +4,8 @@ describe UserDecorator do
   let(:team) { create(:team) }
   let(:user) { create(:user, team: team, team_join_time: 6.days.ago) }
   let(:user_without_team) { create(:user) }
+  let(:project) { build_stubbed(:project) }
+  subject { user.decorate }
 
   describe '#days_in_current_team' do
     context 'user without team' do
@@ -12,8 +14,39 @@ describe UserDecorator do
     end
 
     context 'user with team' do
-      subject { user.decorate }
       it { expect(subject.days_in_current_team).to eq(6) }
     end
+  end
+
+  describe '#as_row' do
+    it { expect(subject.as_row).to eq [user.last_name, user.first_name, user.role, user.location] }
+  end
+
+  describe '#project_names' do
+    before do
+      subject.model.stub(:memberships) { [Membership.new(project: project)] }
+    end
+
+    it { expect(subject.project_names).to eq [project.name] }
+  end
+
+  describe '#link' do
+    it { expect(subject.link).to eq h.link_to subject.name, user }
+  end
+
+  describe '#gravatar_url' do
+    let(:user_md5) { Digest::MD5.hexdigest(subject.email.downcase) }
+    it { expect(subject.gravatar_url).to eq "https://www.gravatar.com/avatar/#{user_md5}?size=80" }
+  end
+
+  describe '#gravatar_image' do
+    it { expect(subject.gravatar_image(size: 80)).to include(subject.gravatar_url) }
+  end
+
+  describe '#days_in_current_team' do
+    before do
+      user.team_join_time = 1.day.ago
+    end
+    it { expect(subject.days_in_current_team).to eq 1 }
   end
 end
