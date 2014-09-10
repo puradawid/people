e430fc0326448ffd19540f30d3ad4ccd71dfc74c
zzk <zzk@chickenkiller.org>
2014-09-10 14:00:30 +0200

    Add missing spec

---
+++ spec/decorators/role_decorator_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe RoleDecorator do
+  let(:role) { create(:role).decorate }
+
+  describe '#label' do
+    it { expect(role.label).to eq("<span class=\"label label-default\" style=\"background-color: \">#{role.name}</span>") }
+  end
+end
