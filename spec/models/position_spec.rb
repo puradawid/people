d88ed1f9dac4756eb3f48e14305695bd0949b2aa
zzk <zzk@chickenkiller.org>
2014-09-10 12:45:36 +0200

    c8ec5c7e93df18d1a93af6f4bc959847db73d68b fix

--- spec/models/position_spec.rb
+++ spec/models/position_spec.rb
@@ -7,20 +7,20 @@ describe Position do
   it { should belong_to :user }
   it { should be_valid }
 
-  describe "#available roles" do
-    let(:juniorRole) { create(:role, name: "junior", technical: true) }
-    let!(:seniorRole) { create(:role, name: "senior", technical: true) }
+  describe '#available roles' do
+    let(:juniorRole) { create(:role, name: 'junior', technical: true) }
+    let!(:seniorRole) { create(:role, name: 'senior', technical: true) }
     let!(:user) { create(:user, role_id: juniorRole.id) }
 
-    context "available roles" do
+    context 'available roles' do
 
-      it "returns available roles when user has no possition assigned" do
+      it 'returns available roles when user has no possition assigned' do
         pos = create(:position, user: user)
         expect(pos.available_roles).to include juniorRole
         expect(pos.available_roles).to include seniorRole
       end
 
-      it "returns available roles when user has possition assigned" do
+      it 'returns available roles when user has possition assigned' do
         create(:position, user: user, role: juniorRole, starts_at: Time.now)
         pos = create(:position, user: user)
         expect(pos.available_roles).to include seniorRole
@@ -29,14 +29,14 @@ describe Position do
     end
   end
 
-  describe "#validate chronology" do
-    let(:juniorRole) { create(:role, name: "junior", technical: true) }
-    let!(:seniorRole) { create(:role, name: "senior", technical: true) }
+  describe '#validate chronology' do
+    let(:juniorRole) { create(:role, name: 'junior', technical: true) }
+    let!(:seniorRole) { create(:role, name: 'senior', technical: true) }
     let!(:user) { create(:user, role_id: juniorRole.id) }
     let!(:juniorPos) { create(:position, user: user, role: juniorRole, starts_at: Date.new(2014, 5, 14)) }
-    context "validate chronology" do
+    context 'validate chronology' do
 
-      it "return error when dates are wrong" do
+      it 'return error when dates are wrong' do
         pos = build(:position, user: user, role: seniorRole, starts_at: juniorPos.starts_at - 2.days)
         expect(pos).not_to be_valid
         expect(pos.errors).not_to be_blank
@@ -44,7 +44,7 @@ describe Position do
         expect(pos.errors[:starts_at]).to include I18n.t('positions.errors.chronology')
       end
 
-      it "should not return error when dates chronology is ok" do
+      it 'should not return error when dates chronology is ok' do
         senior_pos = build(:position, user: user, role: seniorRole, starts_at: juniorPos.starts_at + 2.days)
         expect(senior_pos).to be_valid
         expect(senior_pos.errors).to be_blank
@@ -52,28 +52,32 @@ describe Position do
     end
   end
 
-  describe "#by_user_name_and_date" do
-    let(:pos1) { mock_model(Position, user: nil, role: nil)  }
-    let(:pos2) { mock_model(Position, user: nil, role: nil)  }
-    let(:pos3) { mock_model(Position, user: nil, role: nil)  }
+  describe '#by_user_name_and_date' do
+    let(:pos1) { mock_model(described_class, user: nil, role: nil)  }
+    let(:pos2) { mock_model(described_class, user: nil, role: nil)  }
+    let(:pos3) { mock_model(described_class, user: nil, role: nil)  }
     let(:positions) { [pos1, pos2, pos3] }
-    context "by_user_name_and_date" do
+    context 'by_user_name_and_date' do
 
-      it "not return error when user is nil" do
-        expect(Position.by_user_name_and_date(positions)).to eq([])
+      it 'not return error when user is nil' do
+        expect(described_class.by_user_name_and_date(positions)).to eq([])
       end
-      context "sorts positions" do
+      context 'sorts positions' do
         let(:new_role) { build(:role, name: 'new', technical: true) }
         let(:first_user) { build(:user, first_name: 'Andrew', last_name: 'Snow', role_id: new_role.id) }
         let(:second_user) { build(:user, first_name: 'Tony', last_name: 'Second', role_id: new_role.id) }
-        let(:first_pos) { build(:position, user: first_user, role: new_role, 
-          starts_at: Date.new(2014, 7, 14)) }
-        let(:second_pos) { build(:position, user: second_user, role: new_role, 
-          starts_at: Date.new(2014, 6, 11)) }
+        let(:first_pos) do
+          build(:position, user: first_user, role: new_role,
+          starts_at: Date.new(2014, 7, 14))
+        end
+        let(:second_pos) do
+          build(:position, user: second_user, role: new_role,
+          starts_at: Date.new(2014, 6, 11))
+        end
         let(:all_pos) { [first_pos, second_pos] }
 
-        it "in the right order" do
-          expect(Position.by_user_name_and_date(all_pos)).to eq([second_pos,first_pos])
+        it 'in the right order' do
+          expect(described_class.by_user_name_and_date(all_pos)).to eq([second_pos, first_pos])
         end
       end
     end
