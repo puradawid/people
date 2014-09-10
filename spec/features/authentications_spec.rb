17515c2fc75238a5c2b8e165dca91d6cebd33a32
zzk <zzk@chickenkiller.org>
2014-09-10 12:59:31 +0200

    56c3f90291795abdc331d7a8b557073c17d03544 fix

--- spec/features/authentications_spec.rb
+++ spec/features/authentications_spec.rb
@@ -8,18 +8,17 @@ describe 'Authentication' do
   context 'sign in' do
     let(:user) { build(:user, first_name: 'John', last_name: 'Doe', email: 'jdoe@sth.com') }
     before do
-      OmniAuth.config.add_mock(:google_oauth2, {
-        info: {
-          first_name: user.first_name,
-          last_name: user.last_name,
-          email: user.email
-        },
-        extra: { raw_info: { hd: 'example.com' }},
-        credentials: {
-          oauth_token: 123,
-          refresh_token: 456,
-          oauth_expires_at: Time.now+1.hour
-        }
+      OmniAuth.config.add_mock(:google_oauth2,
+      info: {
+        first_name: user.first_name,
+        last_name: user.last_name,
+        email: user.email
+      },
+      extra: { raw_info: { hd: 'example.com' } },
+      credentials: {
+        oauth_token: 123,
+        refresh_token: 456,
+        oauth_expires_at: Time.now + 1.hour
       })
     end
 
@@ -30,11 +29,11 @@ describe 'Authentication' do
         click_link_or_button 'Sign up with Google'
       end
 
-      it {should have_content('John Now please connect your GitHub account.')}
+      it { should have_content('John Now please connect your GitHub account.') }
 
       context 'and github account' do
         before do
-          OmniAuth.config.add_mock(:github, { info: { nickname: 'xyz' }})
+          OmniAuth.config.add_mock(:github, info: { nickname: 'xyz' })
           click_link_or_button 'connect'
         end
 
@@ -46,7 +45,7 @@ describe 'Authentication' do
   end
 
   context 'sign in without GH account' do
-    let(:second_usr) { create(:user, first_name: 'Greg', gh_nick: nil, last_name: 'Doe', email: 'grdoe@sth.com', without_gh: true ) }
+    let(:second_usr) { create(:user, first_name: 'Greg', gh_nick: nil, last_name: 'Doe', email: 'grdoe@sth.com', without_gh: true) }
     before { sign_in(second_usr) }
 
     it 'redirects to the dashboard' do
