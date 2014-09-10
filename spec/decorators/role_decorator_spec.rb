0462ac66dcc4ee7795706d1b0eed179dae6f0cda
zzk <zzk@chickenkiller.org>
2014-09-10 14:11:44 +0200

    Use html_safe

--- spec/decorators/role_decorator_spec.rb
+++ spec/decorators/role_decorator_spec.rb
@@ -4,6 +4,6 @@ describe RoleDecorator do
   let(:role) { create(:role).decorate }
 
   describe '#label' do
-    it { expect(role.label).to eq("<span class=\"label label-default\" style=\"background-color: \">#{role.name}</span>") }
+    it { expect(role.label).to eq("<span class=\"label label-default\" style=\"background-color: \">#{role.name.html_safe}</span>") }
   end
 end
