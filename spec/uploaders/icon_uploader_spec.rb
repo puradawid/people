fc560c6357f7a330237dfb10d7b16b8e8e41ae3d
Marek Buszman <mbusiq@gmail.com>
2014-09-11 12:38:51 +0200

    icon uploader spec

---
+++ spec/uploaders/icon_uploader_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+require 'carrierwave/test/matchers'
+
+describe IconUploader do
+  let(:ability) { build_stubbed(:ability) }
+  subject { described_class.new(ability, 'image') }
+
+  describe '#store_dir' do
+    it { expect(subject.store_dir).to eq "uploads/ability/image/#{ability.id.to_s}" }
+  end
+
+  describe '#extension_white_list' do
+    it { expect(subject.extension_white_list).to eq %w(jpg jpeg gif png) }
+  end
+end
