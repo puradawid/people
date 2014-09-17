3f490637355a7ff52ddc5b6625b7b1a030ec0354
zzk <zzk@chickenkiller.org>
2014-09-17 10:10:07 +0200

    Touch up searchform spec

--- spec/features/searchform_spec.rb
+++ spec/features/searchform_spec.rb
@@ -7,23 +7,22 @@ describe "User sign up", js: true do
 
   before(:each) do
     page.set_rack_session 'warden.user.user.key' => User.serialize_into_session(user).unshift('User')
-    create(:project, name: "test")   
+    create(:project, name: "test")
     create(:project, name: "zztop")
     visit '/projects'
   end
 
   it "return only matched projects when project name provided" do
-    fill_in 'search', :with => 'test'
+    fill_in 'search', with: 'test'
     click_button 'search'
     expect(page).to have_text('test')
     page.should have_no_content('zztop')
   end
 
   it "return all projects when empty string provided" do
-    fill_in 'search', :with => ""
+    fill_in 'search', with: ""
     click_button 'search'
     expect(page).to have_text('zztop')
     expect(page).to have_text('test')
   end
 end
-
