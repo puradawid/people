79bb34274a8fc4f0ad272aa67524de3def6e3e83
Marcin Stecki <marcin@netguru.pl>
2014-10-13 13:17:58 +0200

    gateway

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,4 @@
 require 'net/ssh/proxy/command'
 server ENV['PRODUCTION_SERVER'], user: ENV['PRODUCTION_USER'], roles: %w{web app db}
+set :ssh_options, proxy: Net::SSH::Proxy::Command.new("ssh #{ENV['GATEWAY']} -W %h:%p")
 set :branch, 'master'
