c61313e14a0221f61b9e6d41321f270f32ee5f4e
Marcin Stecki <marcin@netguru.pl>
2014-10-13 15:19:51 +0200

    fix address

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 lock '3.2.1'
 
 set :application, "people"
-set :repo_url,  "git://github.com:netguru/people.git"
+set :repo_url,  "git://github.com/netguru/people.git"
 set :deploy_to, ENV['DEPLOY_PATH']
 
 set :linked_files, %w{config/mongoid.yml config/sec_config.yml}
