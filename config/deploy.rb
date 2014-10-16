9df325b91ed7a37220ed639e9c9b1319c8228910
Marcin Ostrowski <marcin.ostrowski@netguru.pl>
2014-10-16 12:58:16 +0200

    Added nginx linked files to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,5 +4,5 @@ set :application, "people"
 set :repo_url,  "ssh://git@github.com/netguru/people.git"
 set :deploy_to, ENV['DEPLOY_PATH']
 
-set :linked_files, %w{config/mongoid.yml config/sec_config.yml}
+set :linked_files, %w{nginx.production.people.conf nginx.staging.people.conf config/mongoid.yml config/sec_config.yml}
 set :linked_dirs, %w{bin log tmp vendor/bundle }
