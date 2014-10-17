f8af7fc812ec9b35d9551cececa3b4d255b149a9
Marcin Ostrowski <marcin.ostrowski@netguru.pl>
2014-10-17 15:04:22 +0200

    Added feature check to scheduler

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,6 @@
 # # You can't run whenever, crontabs on Heroku, need to use scheduler: https://scheduler.heroku.com/dashboard
 
 every 1.day, at: '12 am' do
+  return unless Flip.on? :vacations
   rake "people:vacation_checker"
 end
