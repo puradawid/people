ca8f37f0f0890a187d287ed681bd2112ed302940
Marcin Ostrowski <marcin.ostrowski@netguru.pl>
2014-10-17 17:56:56 +0200

    Changed the way user if searched in checker job

--- app/jobs/availability_checker_job.rb
+++ app/jobs/availability_checker_job.rb
@@ -2,7 +2,7 @@ class AvailabilityCheckerJob
   include SuckerPunch::Job
 
   def perform user_id
-    user = User.find(user_id)
+    user = User.where(id: user_id).first
     AvailabilityChecker.new(user).run! if user.present?
   end
 end
