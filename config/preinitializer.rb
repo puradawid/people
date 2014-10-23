4bb6b72b3d14258a1c5fa2227731ca11fe12163d
Marcin Ostrowski <marcin.ostrowski@netguru.pl>
2014-10-23 15:01:40 +0200

    Using config preinitializer in whenever job list

--- config/preinitializer.rb
+++ config/preinitializer.rb
@@ -1,7 +1,14 @@
 require 'active_support/core_ext'
 require 'konf'
 
-pub_config = (YAML.load(ERB.new(File.read(File.expand_path('../config.yml',     __FILE__))).result)[Rails.env] rescue {}) || {}
-sec_config = (YAML.load(ERB.new(File.read(File.expand_path('../sec_config.yml', __FILE__))).result)[Rails.env] rescue {}) || {}
+rails_env =
+  if defined? Rails
+    Rails.env
+  else
+    ENV['RAILS_ENV']
+  end
+
+pub_config = (YAML.load(ERB.new(File.read(File.expand_path('../config.yml',     __FILE__))).result)[rails_env] rescue {}) || {}
+sec_config = (YAML.load(ERB.new(File.read(File.expand_path('../sec_config.yml', __FILE__))).result)[rails_env] rescue {}) || {}
 AppConfig = Konf.new(pub_config.deep_merge(sec_config))
 
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,8 @@
-require 'konf'
-AppConfig = Konf.new('config/config.yml', @environment)
+# Since Rails is not initialized in whenever job file
+# it's necessary to manualy require config yml files
+ENV['RAILS_ENV'] ||= @environment
+require './config/preinitializer'
+
 changes_digest_day = AppConfig.emails.notifications.changes_digest.weekday.to_sym
 
 set :output, 'log/whenever.log'
