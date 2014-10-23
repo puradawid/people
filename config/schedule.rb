7206ab5c4efbeb3887f3e71a98cf3c607fbb5fb6
Marcin Ostrowski <marcin.ostrowski@netguru.pl>
2014-10-23 13:18:08 +0200

    Added output loggin to whenever and sample task

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,9 @@
+set :output, 'log/whenever.log'
+
 every 1.day, at: '12 am' do
   rake "people:vacation_checker"
 end
 
-every :monday, at: '8am' do
+every 1.minute do
   rake 'mailer:changes_digest'
 end
