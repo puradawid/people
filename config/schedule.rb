210b1d0c1a489667e824816d0ecae95268936a1d
Marcin Ostrowski <marcin.ostrowski@netguru.pl>
2014-10-23 14:25:06 +0200

    Added appconfig to whenever job list

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,13 @@
+require 'konf'
+AppConfig = Konf.new('config/config.yml', @environment)
+changes_digest_day = AppConfig.emails.notifications.changes_digest.weekday.to_sym
+
 set :output, 'log/whenever.log'
 
 every 1.day, at: '12 am' do
-  rake "people:vacation_checker"
+  rake 'people:vacation_checker'
 end
 
-every 1.minute do
+every changes_digest_day, at: '8 am' do
   rake 'mailer:changes_digest'
 end
