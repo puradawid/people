c9d4ec5e118230d65465ef4e3ab78cca4fff8c14
Marcin Ostrowski <marcin.ostrowski@netguru.pl>
2014-10-24 11:32:07 +0200

    Added js compressor on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,33 +1,17 @@
 Hrguru::Application.configure do
-
   config.cache_classes = true
-
   config.eager_load = true
-
   config.consider_all_requests_local       = false
-
   config.action_controller.perform_caching = true
-
   config.serve_static_assets = false
-
+  config.assets.js_compressor = :uglifier
   config.assets.compile = false
-
-  config.assets.compress = true
-
   config.assets.digest = true
-
   config.assets.version = '1.0'
-
   config.log_level = :info
-
   config.i18n.fallbacks = true
-
-  config.assets.initialize_on_precompile = false
-
   config.active_support.deprecation = :notify
-
   config.log_formatter = ::Logger::Formatter.new
-
   config.action_mailer.default_url_options = { host: AppConfig.domain }
 
   ActionMailer::Base.smtp_settings = {
