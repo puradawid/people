b8665147edcb19b5a0ec5553c83f9829d9fea053
Marcin Ostrowski <marcin.ostrowski@netguru.pl>
2014-10-27 18:54:57 +0100

    Added assets compressor to config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,6 +5,7 @@ Hrguru::Application.configure do
   config.action_controller.perform_caching = true
   config.serve_static_assets = false
   config.assets.js_compressor = :uglifier
+  config.assets.compress = true
   config.assets.compile = false
   config.assets.digest = true
   config.assets.version = '1.0'
