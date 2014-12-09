218e11bf4a4bb74f56ccf17c5e81d3771a9c7cff
Marcin Dobrowolski <marcin.dobrowolski@netguru.co>
2014-12-09 12:24:17 +0100

    Remove months_in_project from available people
    Moving this feature to users view
    issues #45

--- app/controllers/available_users_controller.rb
+++ app/controllers/available_users_controller.rb
@@ -2,8 +2,6 @@ class AvailableUsersController < ApplicationController
 
   expose_decorated(:users) { available_users }
   expose_decorated(:roles) { Role.technical }
-  expose(:months_options) { months_select_options }
-  expose(:selected_months) { params[:months] || "0" }
 
   def index; end
 
@@ -12,18 +10,6 @@ class AvailableUsersController < ApplicationController
   def available_users
     number_of = (params[:months] || 0).to_i
     @roles = Role.technical.to_a
-    available_technical = User.active.available.roles(@roles)
-    return available_technical unless number_of > 0
-    in_a_project = User.in_a_project_for_over(number_of.months)
-    (available_technical.to_a + in_a_project.to_a).uniq(&:_id)
-  end
-
-  def months_select_options
-    options = { "Don't show" => 0 }
-    (1..12).each do |n|
-      key = n == 1 ? "#{n} month" : "#{n} months"
-      options.merge!( key => n )
-    end
-    options
+    User.active.available.roles(@roles)
   end
 end
