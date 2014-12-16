cf300e0a01459d2a6c32706adb2ba004b9eddab4
Marcin Dobrowolski <marcin.dobrowolski@netguru.co>
2014-12-16 12:27:10 +0100

    Add database.yml to deploy.rb
    [#84378706]

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,5 +6,5 @@ set :deploy_to, ENV['DEPLOY_PATH']
 
 set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
 
-set :linked_files, %w(config/mongoid.yml config/sec_config.yml)
+set :linked_files, %w(config/mongoid.yml config/sec_config.yml config/database.yml)
 set :linked_dirs, %w(bin log tmp vendor/bundle public/uploads)
