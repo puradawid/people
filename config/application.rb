ca21a53ed41168a73c4c3d7973c3275f327b38e7
Marcin Dobrowolski <marcin.dobrowolski@netguru.co>
2014-12-16 10:25:20 +0100

    Add active_record to application.rb
    [#84378706]

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require "action_controller/railtie"
 require "action_mailer/railtie"
+require "active_record/railtie"
 require "sprockets/railtie"
 require "bson"
 require "moped"
