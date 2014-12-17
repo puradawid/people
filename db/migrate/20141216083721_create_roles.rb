dcdac6f48517a4c4cf90fb1a0c5b783c2823c258
Marcin Dobrowolski <marcin.dobrowolski@netguru.co>
2014-12-17 09:32:01 +0100

    [929f75117419f75a574741fb22825cd5dd3ab40b]
    [#84378706]

--- db/migrate/20141216083721_create_roles.rb
+++ db/migrate/20141216083721_create_roles.rb
@@ -1,8 +1,8 @@
 class CreateRoles < ActiveRecord::Migration
   def change
     create_table :roles do |t|
-      t.string :name, type: String
-      t.string :color, type: String
+      t.string :name
+      t.string :color
       t.boolean :billable, default: false
       t.boolean :technical, default: false
       t.boolean :show_in_team, default: true
