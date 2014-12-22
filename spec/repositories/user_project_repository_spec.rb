8e95846751f4ce07096e32fdaf6475d766f4e956
Marcin Dobrowolski <marcin.dobrowolski@netguru.co>
2014-12-22 17:00:49 +0100

    User Project Repository spec
    [#84378748]

---
+++ spec/repositories/user_project_repository_spec.rb
@@ -0,0 +1,56 @@
+require 'spec_helper'
+
+describe UserProjectRepository do
+  subject { described_class.new(user) }
+  let!(:user) { create(:user) }
+
+  describe '#potential' do
+    let!(:potential_project) { create(:project, potential: true) }
+    let!(:potential_membership) { create(:membership, user: user, project: potential_project) }
+    let!(:not_potential_membership) { create(:membership, user: user) }
+
+    it 'returns potential projects that the user has memberships in' do
+      expect(subject.potential.items.to_a).to eq [potential_project]
+    end
+  end
+
+  describe '#next' do
+    let!(:next_project) { create(:project, archived: false, potential: false) }
+    let!(:next_membership) do
+      create(:membership,
+        user: user, starts_at: Time.local(2014, 12, 30), ends_at: Time.local(2015, 1, 30),
+        project: next_project)
+    end
+    let!(:not_next_membership) { create(:membership, user: user) }
+
+    before { Timecop.freeze(2014, 12, 1) }
+
+    it 'returns next projects that the user has memberships in' do
+      expect(subject.next.items.to_a).to eq [next_project]
+    end
+  end
+
+  describe '#current' do
+    let!(:current_project) { create(:project, archived: false, potential: false) }
+    let!(:current_membership) do
+      create(:membership,
+        user: user, starts_at: Time.local(2014, 11, 30), ends_at: Time.local(2014, 12, 29),
+        project: current_project)
+    end
+    let!(:not_current_membership) { create(:membership) }
+
+    before { Timecop.freeze(2014, 12, 1) }
+
+    it 'returns current proects that the user has memberships in' do
+      expect(subject.current.items.to_a).to eq [current_project]
+    end
+  end
+
+  describe '#items' do
+    let!(:membership) { create(:membership) }
+
+    it 'returns ProjectSearch results' do
+      expect(subject.items.to_a).to eq ProjectSearch.new(memberships: [membership]).results.to_a
+    end
+  end
+end
