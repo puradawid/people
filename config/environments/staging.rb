8ed804912c02b3cfb906351c6925d48adb6ae4fb
Marcin Ostrowski <marcin.ostrowski@netguru.pl>
2015-01-09 12:13:06 +0100

    Rise log level on stg [#85769540]

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -10,10 +10,9 @@ Hrguru::Application.configure do
   config.assets.compile = false
   config.assets.digest = true
   config.assets.version = '1.0'
-  config.log_level = :info
+  config.log_level = :debug
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
-  config.log_formatter = ::Logger::Formatter.new
   config.action_mailer.default_url_options = { host: AppConfig.domain }
   GA.tracker = 'UA-35395053-13'
 
