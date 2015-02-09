d81789a52ce9700ef385cb1df75ff8904eb60ec5
Michał Małowski <michal.malowski@netguru.co>
2015-02-09 15:29:59 +0100

    set proper roles as technical

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@ Dir[Rails.root.join("spec/factories/*.rb")].each {|f| require f}
 
 billable_roles = %w(senior developer)
 non_billable_roles = %w(junior praktykant pm junior\ pm qa junior\ qa)
+technical_roles = %w(junior praktykant developer senior)
 admin_roles = %w(senior pm)
 contract_types = %w(DG UoP UoD)
 locations = %w(Poznan Warsaw Gdansk Zielona\ Gora Krakow Remotely)
@@ -15,6 +16,10 @@ end
 
 non_billable_roles.each do |name|
   Role.find_or_create_by(name: name).update_attribute(:billable, false)
+end
+
+technical_roles.each do |name|
+  Role.find_or_create_by(name: name).update_attribute(:technical, true)
 end
 
 admin_roles.each do |name|
