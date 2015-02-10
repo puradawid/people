5dfff62b4437b028f0994ea4a27a43e80e6d6b44
Michał Małowski <michal.malowski@netguru.co>
2015-02-10 13:31:34 +0100

    remove lograge gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,7 +28,6 @@ Hrguru::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-  config.lograge.enabled = true
   config.after_initialize do
     Bullet.enable = true
     Bullet.bullet_logger = true
