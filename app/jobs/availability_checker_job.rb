60c1980ffde86bf0c268244af630656ff0549b07
Michał Małowski <michal.malowski@netguru.co>
2015-02-11 22:24:51 +0100

    fix codestyle

--- app/jobs/availability_checker_job.rb
+++ app/jobs/availability_checker_job.rb
@@ -1,8 +1,11 @@
 class AvailabilityCheckerJob
   include SuckerPunch::Job
 
-  def perform user_id
+  def perform(user_id)
     user = User.where(id: user_id).first
-    AvailabilityChecker.new(user).run! if user.present?
+
+    if user.present?
+      AvailabilityChecker.new(user).run!
+    end
   end
 end
