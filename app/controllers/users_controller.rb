213fa1b3ba8e4daa05fdc8a1c051bc7fda4374ac
dalach <wiktoria.dalach@netguru.pl>
2015-02-12 12:41:29 +0100

    fix user/show virew and users_controler_spec [#87712360]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -71,7 +71,7 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :role_id, :admin_role_id, :team_id, :leader_team_id,
+    params.require(:user).permit(:first_name, :last_name, :primary_role_id, :admin_role_id, :team_id, :leader_team_id,
       :employment, :phone, :location_id, :contract_type_id, :user_notes,
       :archived, :skype, abilities_names: [])
   end
