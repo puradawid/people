b09f390e8f3f116bb2708cd6a977b4aa638ea5ef
Michał Małowski <michal.malowski@netguru.co>
2015-02-20 14:18:53 +0100

    add specs for setting correct icons as visible
    [#88035858]

--- spec/factories/project.rb
+++ spec/factories/project.rb
@@ -3,6 +3,7 @@ FactoryGirl.define do
     sequence(:name) { |i| "#{Faker::Internet.domain_word}_#{i}" }
     slug { name.try(:downcase).try(:tr, '^a-z0-9', '') }
     end_at { 30.days.from_now }
+    archived false
 
     factory :project_deleted do
       deleted_at Time.now
@@ -10,6 +11,14 @@ FactoryGirl.define do
 
     factory :invalid_project do
       name nil
+    end
+
+    trait :potential do
+      potential true
+    end
+
+    trait :archived do
+      archived true
     end
   end
 end
---
+++ spec/features/projects_spec.rb
@@ -0,0 +1,60 @@
+require 'spec_helper'
+
+describe 'Projects page', js: true do
+  let(:senior_role) { create(:admin_role) }
+  let!(:user) { create(:user, admin_role_id: senior_role.id) }
+  let!(:active_project) { create(:project) }
+  let!(:potential_project) { create(:project, :potential) }
+  let!(:archived_project) { create(:project, :archived) }
+
+  before do
+    sign_in(user)
+    visit '/dashboard' # Projects tab
+  end
+
+  describe 'tabs' do
+    it 'has Active/Potential/Archived tabs' do
+      within('#filters') do
+        page.find('li.active').click
+        page.find('li.potential').click
+        page.find('li.archived').click
+      end
+    end
+  end
+
+  describe 'project row' do
+    context 'when on Active tab' do
+      it 'displays action icons (timelapse) when hovered' do
+        within('#filters') { page.find('li.active').click }
+
+        within('.project') do
+          expect(page.find('.unarchive', visible: false)).to_not be_visible
+          expect(page.find('.archive')).to be_visible
+          expect(page.find('.info.js-timeline-show')).to be_visible
+        end
+      end
+    end
+
+    context 'when on Potential tab' do
+      it 'displays action icons (archive, timelapse) when hovered' do
+        page.find('li.potential').click
+        within('.project.potential') do
+          expect(page.find('.unarchive', visible: false)).to_not be_visible
+          expect(page.find('.archive', visible: false)).to_not be_visible
+          expect(page.find('.info.js-timeline-show')).to be_visible
+        end
+      end
+    end
+
+    context 'when on Archived tab' do
+      it 'displays action icons (unarchive, timelapse) when hovered' do
+        page.find('li.archived').click
+        within('.project.archived') do
+          expect(page.find('.unarchive')).to be_visible
+          expect(page.find('.archive', visible: false)).to_not be_visible
+          expect(page.find('.info.js-timeline-show')).to be_visible
+        end
+      end
+    end
+  end
+end
