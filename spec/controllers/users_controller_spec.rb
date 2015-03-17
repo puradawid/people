a31029e6f245620a2c8038a6b4aa1ab744752586
Aleksandra Nabożny <aleksandra.nabozny@netguru.pl>
2015-03-17 13:27:37 +0100

    modify users controller spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,7 +7,7 @@ describe UsersController do
     before do
       sign_in create(:user, first_name: 'Marian')
       create(:user, first_name: 'Tomek')
-      create(:user, first_name: 'Anna', archived: true)
+      create(:user, first_name: 'Ziuta', archived: true)
     end
 
     it 'responds successfully with an HTTP 200 status code' do
@@ -25,7 +25,7 @@ describe UsersController do
       get :index
       expect(response.body).to match(/Marian/)
       expect(response.body).to match(/Tomek/)
-      expect(response.body).not_to match(/Anna/)
+      expect(response.body).not_to match(/Ziuta/)
     end
   end
 
