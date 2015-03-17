83a2fd58c711cc36e25e157c81336545aae52445
Marcin Ostrowski <marcin.ostrowski@netguru.pl>
2015-03-17 15:02:57 +0100

    Spec for counting devs and jnrs [#88040398]

--- spec/features/teams_spec.rb
+++ spec/features/teams_spec.rb
@@ -7,7 +7,7 @@ describe 'team view', js: true do
   let(:hidden_role) { create(:role, show_in_team: false)}
   let(:user) { create(:user, admin_role_id: admin_role.id) }
   let!(:dev_role) { create(:role, name: 'developer') }
-  let!(:junior_role) { create(:role, name: 'junior') }
+  let!(:junior_role) { create(:role, name: 'junior', billable: false) }
   let!(:dev_user) { create(:user, first_name: 'Developer Daisy', admin_role_id: admin_role.id, primary_role: billable_role) }
   let!(:non_dev_user) { create(:user, first_name: 'Nondev Nigel', primary_role: non_dev_role) }
   let!(:archived_user) { create(:user, first_name: 'Archived Arthur', archived: true) }
@@ -15,6 +15,7 @@ describe 'team view', js: true do
   let!(:hidden_user) { create(:user, first_name: 'Hidden Amanda', primary_role: hidden_role, team_id: team.id) }
   let!(:team) { create(:team) }
   let!(:team_user) { create(:user, first_name: 'Developer Dave', primary_role: billable_role, team_id: team.id) }
+  let!(:junior_team_user) { create(:user, first_name: 'Junior Jake', primary_role: junior_role, team_id: team.id) }
 
   before(:each) do
     page.set_rack_session 'warden.user.user.key' => User.serialize_into_session(user).unshift('User')
@@ -74,6 +75,16 @@ describe 'team view', js: true do
       find('input.new-name').set('Relatively OK team')
       find('button.edit-team-submit').click
       expect(page).to have_content('Relatively OK team')
+    end
+  end
+
+  describe '.devs-indicator' do
+    it 'shows number of users in team' do
+      indicator = first('.devs-indicator')
+      devs_indicator = indicator.first('.devs').text
+      jnrs_indicator = indicator.first('.jnrs').text
+      expect(devs_indicator).to eq '1'
+      expect(jnrs_indicator).to eq '1'
     end
   end
 end
