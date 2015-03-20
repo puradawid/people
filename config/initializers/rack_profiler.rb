f167e7e2cfe22285fce55167fed4314b254830aa
Adam Nowak <lubieniebieski@gmail.com>
2015-03-20 23:12:21 +0100

    enable rack mini profiler

--- config/initializers/rack_profiler.rb
+++ config/initializers/rack_profiler.rb
@@ -1,4 +1,4 @@
 if %w(development staging).include?(Rails.env)
   require 'rack-mini-profiler'
-  #Rack::MiniProfilerRails.initialize!(Rails.application)
+  Rack::MiniProfilerRails.initialize!(Rails.application)
 end
