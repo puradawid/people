e4aaf07ebc95f069b1c2b00c5d1589bd0c74db32
Daniel <kadyceusz@gmail.com>
2015-03-21 17:22:16 +0100

    First test from warzywo :)

--- spec/features/user_profile_spec.rb
+++ spec/features/user_profile_spec.rb
@@ -2,19 +2,36 @@ require 'spec_helper'
 
 describe 'profile', js: true do
   let(:user) { create(:junior_dev) }
+  let!(:junior_role) { create(:junior_role) }
+  let!(:pm_role) { create(:pm_role) }
+  let!(:role_billable) { create(:role_billable) }
 
   before do
     page.set_rack_session 'warden.user.user.key' => User.serialize_into_session(user).unshift('User')
-    create(:junior_role)
-    create(:pm_role)
-    create(:role_billable)
-    visit user_path(user.id)
   end
 
   describe 'setting role' do
+    before do
+      user.roles = [junior_role, pm_role, role_billable]
+      user.save
+      visit user_path(user.id)
+    end
+
+    it 'set role to a user' do
+      expect(page).to have_select('js-user-primary', selected: 'no role')
+      select('junior', from: 'js-user-primary')      
+      within('form.edit_user') do
+        first('input[type=submit]').click
+      end
+      expect(page).to have_select('js-user-primary', selected: 'junior')
+    end
   end
 
   describe 'adding roles' do
+    before do
+      visit user_path(user.id)
+    end
+
     it 'adds role to user' do
       expect(page).to have_select('js-user-primary', options: ['no role'])
       select('junior', from: 'js-user-roles')
@@ -25,4 +42,8 @@ describe 'profile', js: true do
       expect(page).to have_select('js-user-primary', options: ['no role', 'junior', 'pm'])
     end
   end
+
+
+
+
 end
