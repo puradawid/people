6486403a9fb1913879d5a0287f9c86e1a9b93b6a
Błażej Pankowiak <blazej.pankowiak@netguru.pl>
2015-03-23 18:05:08 +0100

    Exclude non-technical people from AvailableUsers [#90921824]

--- app/models/available_users.rb
+++ app/models/available_users.rb
@@ -1,5 +1,12 @@
 class AvailableUsers
   def all
     User.where(available: true)
+      .where(:primary_role.in => technical_roles)
+  end
+
+  private
+
+  def technical_roles
+    Role.where(technical: true).pluck(:id)
   end
 end
--- spec/models/available_users_spec.rb
+++ spec/models/available_users_spec.rb
@@ -1,11 +1,18 @@
 describe AvailableUsers do
-  let(:available_user) { create(:user, :available) }
-  let(:unavailable_user) { create(:user) }
+  let!(:available_user) { create(:user, :available) }
+  let!(:unavailable_user) { create(:user) }
+  let!(:project_manager) do
+    create(:user, :available, primary_role: create(:pm_role))
+  end
 
   subject { AvailableUsers.new.all }
 
   it 'returns only available users' do
     expect(subject).to include available_user
     expect(subject).not_to include unavailable_user
+  end
+
+  it 'returns only technical users' do
+    expect(subject).not_to include project_manager
   end
 end
