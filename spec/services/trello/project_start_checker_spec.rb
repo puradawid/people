a2cf8a54d7349067bcf0adc5dd701c237d6ff15d
Błażej Pankowiak <blazej.pankowiak@netguru.pl>
2015-03-26 18:18:26 +0100

    Do not create a new membership when user is already in the project [#90918316]

--- spec/services/trello/project_start_checker_spec.rb
+++ spec/services/trello/project_start_checker_spec.rb
@@ -8,6 +8,12 @@ describe Trello::ProjectStartChecker do
   let(:key) { AppConfig.trello.developer_public_key }
   let(:token) { AppConfig.trello.member_token }
   let!(:user) { create(:user, first_name: 'John', last_name: 'Doe') }
+  let!(:user_with_membership) do
+    create(:user, first_name: 'Other', last_name: 'Developer')
+  end
+  let!(:project) { create(:project, name: 'secondproject') }
+  let!(:membership) { create(:membership, project: project, user: user_with_membership) }
+
 
   before do
     stub_request(
@@ -44,6 +50,12 @@ describe Trello::ProjectStartChecker do
     it 'creates a membership with role set to current user position' do
       subject.run!
       expect(user.memberships.first.role).to eq user.primary_role
+    end
+  end
+
+  context 'user card with existing label' do
+    it 'does not create a new membership for the user' do
+      expect{ subject.run! }.to_not change{ user_with_membership.memberships.count }.by 1
     end
   end
 end
