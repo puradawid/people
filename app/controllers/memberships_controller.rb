3e179d48275893ce7fe0d2141622958b46c9ad8c
Pura Dawid <puradawid@gmail.com>
2015-03-28 12:53:15 +0100

    Formatting in membership_controller.

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -8,9 +8,14 @@ class MembershipsController < ApplicationController
   expose_decorated(:memberships) { memberships_repository.all }
   expose_decorated(:projects) { projects_repository.all_by_name }
   expose_decorated(:roles) { roles_repository.all_by_name }
-  expose_decorated(:users) { current_user.admin? ? users_repository.all_by_name : [current_user] }
+  expose_decorated(:users) do
+    current_user.admin? ? users_repository.all_by_name : [current_user]
+  end
 
-  before_filter :authenticate_admin!, only: [:index, :update, :destroy, :create, :edit], unless: -> { membership.user == current_user }
+  before_filter :authenticate_admin!,
+                only: [:index, :update, :destroy, :create, :edit],
+                unless: -> { membership.user == current_user }
+
   before_action :set_users_gon, only: [:new, :create]
 
   def index; end
@@ -42,13 +47,14 @@ class MembershipsController < ApplicationController
   protected
 
   def membership_params
-    params.require(:membership)
-      .permit(:starts_at, :ends_at, :project_id, :user_id, :role_id, :billable, :booked)
+    params.require(:membership).
+      permit(:starts_at, :ends_at, :project_id,
+             :user_id, :role_id, :billable, :booked)
   end
 
   private
 
   def set_users_gon
-    gon.rabl template: 'app/views/memberships/users', as: 'users'
+    gon.rabl template: "app/views/memberships/users", as: "users"
   end
 end
